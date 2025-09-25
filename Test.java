f9ec276e59a529df037074cd5d30e35254d5f8c5
Isya-25th <isyaputra.r25th@gmail.com>
2025-09-25 09:15:56 +0700

    Update: file Test.java2

--- Test.java
+++ Test.java
@@ -3,5 +3,6 @@ public class Test {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("Hi Nico");
         System.out.println("Hi Isya");
+        System.out.println("Finish");
     }
 }
